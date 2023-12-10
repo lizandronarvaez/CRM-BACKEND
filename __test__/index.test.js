16e6cbfb4486c874ad99078af399aca9e20e0bdb
Lizandro Narvaez <lizandrojesus13@hotmail.com>
2023-12-10 13:07:18 +0100

    Carpeta test para testeo de endpoint

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -1,26 +1,49 @@
 /* eslint-disable no-undef */
 import axios from "axios";
-const url = "http://localhost:5000/clientes";
+import { config } from "dotenv";
+config({ path: "config.env" });
+const url = `${process.env.URL_TESTING}/clientes`;
 
 describe("Ruta Clientes", () => {
-    // endpoinnt cliente get
+
     test("Devolver un 200 y mostrar datos", async () => {
         const res = await axios.get(url);
-        const { data } = res;
-        console.log(typeof data);
         expect(res.status).toBe(200);
     });
 
-    // endpoint CLIENTE POST
-    test("Crear Cliente y devolver que ha sido creado correcamente", async () => {
+    test("Crea un cliente y devolver que ha sido creado correcamente", async () => {
         const res = await axios.post(url, {
-            nombre: "prueba2",
-            apellido: "prueba2",
-            empresa: "desarrollo web node-mmongodb",
-            email: "correo@example.com",
+            nombre: "Lizandro",
+            apellido: "Narváez",
+            empresa: "Desarollo Company",
+            email: "c@example.com",
             telefono: "612545888"
         });
         expect(res.status).toBe(201);
         expect(res.data).toEqual({ mensaje: "Cliente creado correctamente" });
     });
+
+    // test("Debe enviar un error si el correo ya existe", async () => {
+    //     try {
+    //         await axios.post(url, {
+    //             nombre: "prueba2",
+    //             apellido: "prueba2",
+    //             empresa: "desarrollo web node-mmongodb",
+    //             email: "c@example.com",
+    //             telefono: "612545888"
+    //         });
+    //     } catch (error) {
+    //         expect(error.response.status).toBe(400);
+    //         expect(error.response.data).toEqual({ error: "El correo electrónico ya existe en la base de datos" });
+    //     }
+    // });
+
+    // eliminar un cliente
+    // test("Debe eliminar un cliente", async () => {
+    //     const idCliente = "65597de69a89f36dc03f874f";
+    //     const res = await axios.delete(`${url}/${idCliente}`);
+
+    //     expect(res.data).toEqual("");
+    //     expect(res.status).toBe(204);
+    // });
 });
