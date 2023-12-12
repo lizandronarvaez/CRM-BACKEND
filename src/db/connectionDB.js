862ac839e0205ab07f6b4a4c8fc605a11052effd
Lizandro Narvaez <lizandrojesus13@hotmail.com>
2023-12-12 15:43:46 +0100

    Removiendo dependencias obsoletas

--- src/db/connectionDB.js
+++ src/db/connectionDB.js
@@ -4,17 +4,12 @@ mongoose.Promise = global.Promise;
 const { DATABASE } = getEnv();
 
 export const connectionDB = () => {
-    mongoose.set("strictQuery", "false");
-    // Conexion ala base de datos
+    mongoose.set("strictQuery", true);
     try {
-        mongoose.connect(DATABASE,
-            {
-                useNewUrlParser: true,
-                useUnifiedTopology: true
-            });
+        mongoose.connect(DATABASE);
         console.log("Conectado ala base de datos");
     } catch (error) {
         console.log("Error en la conexion");
-        console.log(error);
+        throw new Error(error);
     }
 };
