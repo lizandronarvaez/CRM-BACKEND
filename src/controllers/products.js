1e6431bef632b6ec7f6d1f1d02b99e6fcd0734e3
Lizandro Narvaez <lizandrojesus13@hotmail.com>
2023-12-14 16:40:36 +0100

    Agregando excepcion de codigo

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -6,20 +6,18 @@ import {
     productGetBy,
     productUpdate,
     productDelete,
-    getAllsProductQuery,
-    uploadFile
+    getAllsProductQuery
 } from "../services/productService.js";
-// instancia de express.router
+import { uploadFile } from "../middlewares/multer.js";
 const route = express.Router();
 route
-
     .post("/", isValidToken, uploadFile, productsCreate)
     .get("/", isValidToken, productsGetAll)
 
     .get("/:_id", isValidToken, productGetBy)
     .put("/:_id", isValidToken, uploadFile, productUpdate)
+    .delete("/:_id", isValidToken, productDelete)
 
-    .get("/clientes/:query", isValidToken, getAllsProductQuery)
-    .delete("/:_id", isValidToken, productDelete);
+    .get("/clientes/:query", isValidToken, getAllsProductQuery);
 
 export default route;
